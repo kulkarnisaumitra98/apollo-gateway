a072117f8ce0de625e2c761cfe123afbad948f07
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2022-01-02 21:22:26 +0530

    update mutation

--- gateway-server/utils.js
+++ gateway-server/utils.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import { print, buildSchema, GraphQLScalarType } from "graphql";
+import { print } from "graphql";
 import { introspectSchema } from "@graphql-tools/wrap";
 import { stitchSchemas } from "@graphql-tools/stitch";
 import { delegateToSchema } from "@graphql-tools/delegate";
@@ -20,16 +20,8 @@ const linkSchemaDef = `
   }
 `;
 
-const uploadSchema = buildSchema(`
-      scalar Upload
-      
-      type Mutation {
-        uploadBookFile(file: Upload!): String
-      }
-`);
-
 export const getRemoteSchema = async (url) => {
-  async function executor({ document, variables }) {
+  let executor = async ({ document, variables }) => {
     const query = print(document);
     const fetchResult = await fetch(url, {
       method: "POST",
@@ -37,6 +29,10 @@ export const getRemoteSchema = async (url) => {
       body: JSON.stringify({ query, variables }),
     });
     return fetchResult.json();
+  };
+
+  if (url === serviceUrls[0]) {
+    executor = linkToExecutor(createServerHttpLink({ uri: url, fetch }));
   }
 
   const schema = {
@@ -52,15 +48,8 @@ export const getRemoteSchemas = async () => {
     serviceUrls.map((serviceUrl) => getRemoteSchema(serviceUrl))
   );
 
-  const uploadSubSchema = {
-    schema: uploadSchema,
-    executor: linkToExecutor(
-      createServerHttpLink({ uri: serviceUrls[0], fetch })
-    ),
-  };
-
   return stitchSchemas({
-    subschemas: [...remoteSchemas.filter(Boolean), uploadSubSchema],
+    subschemas: remoteSchemas.filter(Boolean),
     resolvers: [
       {
         Author: {
